a0f21e60ad968a9a0c28d8c5b3e50d65803a09a4
Alex Pitzel <pitzelalex@gmail.com>
2022-10-27 12:46:27 -0400

    feat: Create Gradebook #list_students

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -10,4 +10,18 @@ class Gradebook
   def add_course(course)
     @courses << course
   end
+
+  def list_students
+    @student_list = []
+    @courses.each do |course|
+      unique_students = course.students.select do |student|
+        !@student_list.include?(student.name)
+      end
+      @student_list << unique_students.map do |student|
+        student.name
+      end
+      @student_list.flatten!
+    end
+    return @student_list
+  end
 end
