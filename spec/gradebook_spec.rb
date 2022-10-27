a3e6603b3e530f20ae7ce295e84c8bb317610e6e
Alex Pitzel <pitzelalex@gmail.com>
2022-10-27 12:24:32 -0400

    test: Create #list_students

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -31,4 +31,38 @@ describe Gradebook do
       expect(gradebook.courses).to eq([course1, course2])
     end
   end
+
+  describe '#list_students' do
+    it 'return a list of all students in all courses in the gradebook' do
+      course1 = Course.new("Calculus", 2)
+      course2 = Course.new("Math", 3)
+      course3 = Course.new("Basket Weaving", 4)
+      gradebook = Gradebook.new("Alex")
+      course1.enroll(Student.new({name: "Morgan", age: 21}))
+      course1.enroll(Student.new({name: "Mike", age: 22}))
+      course2.enroll(Student.new({name: "Morgan", age: 21}))
+      course2.enroll(Student.new({name: "Jamie", age: 22}))
+      course2.enroll(Student.new({name: "Pat", age: 21}))
+      course3.enroll(Student.new({name: "Morgan", age: 21}))
+      course3.enroll(Student.new({name: "Mike", age: 22}))
+      course3.enroll(Student.new({name: "Jamie", age: 22}))
+      course3.enroll(Student.new({name: "Pat", age: 21}))
+      course1.students[0].log_score(98)
+      course1.students[0].log_score(78)
+      course1.students[1].log_score(56)
+      course1.students[1].log_score(77)
+      course2.students[0].log_score(75)
+      course2.students[1].log_score(80)
+      course2.students[2].log_score(50)
+      course3.students[0].log_score(65)
+      course3.students[1].log_score(92)
+      course3.students[2].log_score(88)
+      course3.students[3].log_score(48)
+      gradebook.add_course(course1)
+      gradebook.add_course(course2)
+      gradebook.add_course(course3)
+
+      expect(gradebook.list_students).to eq(["Morgan", "Mike", "Jamie", "Pat"])
+    end
+  end
 end
