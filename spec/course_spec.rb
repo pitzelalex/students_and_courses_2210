1a27d6f6ef1b619f3c15d8c614d7c79f94dce60f
Alex Pitzel <pitzelalex@gmail.com>
2022-10-27 11:44:51 -0400

    test: Create Course #enroll

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -31,4 +31,16 @@ describe Course do
       expect(course.full?).to be false
     end
   end
+
+  describe '#enroll' do
+    it 'add student to @students' do
+      course = Course.new("Calculus", 2)
+      student1 = Student.new({name: "Morgan", age: 21})
+      student2 = Student.new({name: "Jordan", age: 29})
+      course.enroll(student1)
+      course.enroll(student2)
+
+      expect(course.students).to eq([student1, student2])
+    end
+  end
 end
