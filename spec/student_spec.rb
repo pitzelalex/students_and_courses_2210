a06dd833669d734e3bc56f9bc88c9083effb18aa
Alex Pitzel <pitzelalex@gmail.com>
2022-10-27 11:12:40 -0400

    test: Create Student #age 'returns the age'

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -21,4 +21,12 @@ RSpec.describe Student do
       expect(student.name).to eq("Morgan")
     end
   end
+
+  describe '#age' do
+    it 'returns the age from student.details' do
+      student = Student.new({name: "Morgan", age: 21})
+
+      expect(student.age).to eq(21)
+    end
+  end
 end
