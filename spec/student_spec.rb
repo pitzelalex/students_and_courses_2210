a944fbaf3a9849759357febdc60209311fe65c8e
Alex Pitzel <pitzelalex@gmail.com>
2022-10-27 11:03:59 -0400

    test: Create Student #initialize 'is an instance of Student'

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,4 +1,12 @@
 require './lib/student'
 
 RSpec.describe Student do
+  describe '#initialize' do
+  before(:each) do
+    @student = Student.new({name: "Morgan", age: 21})
+  end
+    it 'is an instance of Student' do
+      expect(@student).to be_a Student
+    end
+  end
 end
