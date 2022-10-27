a7962b188074e8a14ea0edbb9522c0748f7c5144
Alex Pitzel <pitzelalex@gmail.com>
2022-10-27 11:35:38 -0400

    test: Add Course #initialize 'has a name'

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -4,11 +4,15 @@ require './lib/course'
 describe Course do
   describe '#initialize' do
     before(:each) do
-      @course = Course.new
+      @course = Course.new("Calculus", 2)
     end
-    
+
     it 'is an instance of Course' do
       expect(@course).to be_a Course
+    end
+
+    it 'has a name' do
+      expect(@course.name).to eq("Calculus")
     end
   end
 end
